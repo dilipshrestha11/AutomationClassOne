252bc5a3617b146ceaca1c35be464ea5414b56f5
dilipshrestha11 <dilipshrestha11@192.168.86.35>
2020-02-02 16:48:56 -0500

    First push

---
+++ AutomationClassOne/src/main/java/com/DataTypePackage/DataType.java
@@ -0,0 +1,16 @@
+package com.DataTypePackage;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x = 1;
+int t = 2;
+double y = 2.5;
+char z = 'A';
+String name="ZTA;"
+//S		
+		
+	}
+
+}
